81901a720cecfecc4cd7c8f06ff4591c85d7eb0e
KissaOlga <olgakissaavk@gmail.com>
2021-05-11 19:38:24 +0300

    1

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import './menu.json';
+import './templates/menu.hbs';
+import './styles.css';
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+  body: document.querySelector('body'),
+  itemMenuCards: document.querySelector('.js-menu'),
+  chekcBox: document.querySelector('#theme-switch-toggle'),
+};
+
+refs.body.classList.add(Theme.LIGHT);
+
+refs.chekcBox.addEventListener('change', onCheckBoxСhange);
+keepsChekcBoxСhange();
+
+const cardsMenu = createCardsMenu(menu);
+refs.itemMenuCards.insertAdjacentHTML('afterbegin', cardsMenu);
+
+function createCardsMenu(menu) {
+  return menuCardsTpl(menu);
+}
+
+function onCheckBoxСhange(e) {
+  const theme = e.target.checked;
+  changeTheme(theme);
+  localStorage.setItem(Key, theme);
+}
+
+function keepsChekcBoxСhange() {
+  const savedТheme = JSON.parse(localStorage.getItem(Key));
+  refs.chekcBox.checked = savedТheme;
+  changeTheme(savedТheme);
+}
+
+function changeTheme(themes) {
+  themes
+    ? refs.body.classList.replace(Theme.LIGHT, Theme.DARK)
+    : refs.body.classList.replace(Theme.DARK, Theme.LIGHT);
+}
